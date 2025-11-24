e89cce6b7121215afb72ef555af01c15f88b69d2
sekong11 <sekong1108@gmail.com>
2025-11-24 17:41:25 +0900

    [Bronze V] Title: A+B - 7, Time: 104 ms, Memory: 13120 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        int i = 1;
+        while (T-- > 0) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println("Case #" + i++ + ": "+(a+b));
+        }
+        sc.close();
+    }
+}
